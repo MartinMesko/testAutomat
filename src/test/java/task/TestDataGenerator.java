86ee958b107fadc4e006058d743c6834504ce38b
MartinMesko <alegremato@gmail.com>
2024-05-20 09:05:06 +0200

    Zmena oddelovaca pomocou File.separator.

--- src/test/java/task/TestDataGenerator.java
+++ src/test/java/task/TestDataGenerator.java
@@ -2,6 +2,7 @@ package task;
 
 import org.junit.jupiter.params.provider.Arguments;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,16 +15,16 @@ public class TestDataGenerator {
 
     public static Stream<Arguments> loadTestData() {
         List<Arguments> argumentsList = new ArrayList<>();
-        String basePath = "src/main/java/odovzdavky";
+        String basePath = "src" + File.separator + "main" + File.separator + "java" + File.separator + "odovzdavky";
 
         try (Stream<Path> paths = Files.walk(Paths.get(basePath))) {
             paths.filter(Files::isRegularFile)
                     .filter(path -> path.toString().endsWith(".java"))
                     .forEach(path -> {
                         String studentPath = path.toString()
-                                .replace("src/main/java/", "")
+                                .replace("src" + File.separator + "main" + File.separator + "java" + File.separator, "")
                                 .replace(".java", "")
-                                .replace("/", ".");
+                                .replace(File.separator, ".");
                         argumentsList.add(Arguments.of(studentPath));
                     });
         } catch (IOException e) {
